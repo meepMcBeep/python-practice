143ae6f601e932d434b5b058a74bbe912a610705
David Barban <david@nabrab.com>
2026-03-27 14:07:31 -0700

    Update Set 2 Assignment 4 - Making global list local, making list split a function, cleanup (Final Version)

--- set02/set02_assignment4_countEvenOdd.py
+++ set02/set02_assignment4_countEvenOdd.py
@@ -7,21 +7,27 @@ Author: David Barban
 Date: 03-27-2026
 Description: from a given list of integers, return lists of even and odd numbers
 """
+
 import random
 
-global_random_list = [random.randint(-100, 100) for number in range(10)]
 
-def main():
-	odd_list = []
+def split_even_odd(numbers):
 	even_list = []
+	odd_list = []
 
-	for number in global_random_list:
+	for number in numbers:
 		if number % 2 == 0:
 			even_list.append(number)
 		else:
 			odd_list.append(number)
+	return even_list, odd_list
 
-	print(f"Original list: {global_random_list}")
+def main():
+	random_list = [random.randint(-100, 100) for _ in range(10)]
+
+	even_list, odd_list = split_even_odd(random_list)
+
+	print(f"Original list: {random_list}")
 	print(f"Odd list: {odd_list}")
 	print(f"Even list: {even_list}")
 
